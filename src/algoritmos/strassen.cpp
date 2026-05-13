c874f27510ff001467b1977477f08224c4781916
marCRACK29 <mamartinez2023@udec.cl>
2026-05-13 15:23:52 -0400

    se incluye caso baso de strassen para un n0 (n>=n0). Una versión hibrida que si no se setea el n0, entonces queda en su versión estandar

--- src/algoritmos/strassen.cpp
+++ src/algoritmos/strassen.cpp
@@ -1,19 +1,12 @@
 #include "../matriz/matriz.h"
-// #include "clasico.cpp"
+#include "clasico.cpp"
 
-Matriz strassen(const Matriz& A, const Matriz& B) {
+Matriz strassen(const Matriz& A, const Matriz& B, int n0=1) {
     int n = A.get_filas();
-    // SUGERENCIA: Si el tamaño es menor o igual a 64, usar multiplicación estándar
-    // Ya que es más eficiente para matrices pequeñas
-    // if (n <= 64) {
-    //     return clasico(A, B);
-    // }
-
-    // CASO BASE: Multiplicación escalar simple
-    if (n == 1) {
-        Matriz C(1, 1);
-        C(0, 0) = A(0, 0) * B(0, 0);
-        return C;
+    // CASO BASE: Si el tamaño es menor o igual a n0, usar multiplicación estándar
+    // en caso de n0 = 1, se usa la versión clásica
+    if (n <= n0) {
+        return clasico(A, B);
     }
 
     // DIVIDIR: Calcular el tamaño de las submatrices
@@ -38,13 +31,13 @@ Matriz strassen(const Matriz& A, const Matriz& B) {
     }
 
     // CONQUISTAR: Las 7 llamadas recursivas de Strassen
-    Matriz M1 = strassen(A11 + A22, B11 + B22);
-    Matriz M2 = strassen(A21 + A22, B11);
-    Matriz M3 = strassen(A11, B12 - B22);
-    Matriz M4 = strassen(A22, B21 - B11);
-    Matriz M5 = strassen(A11 + A12, B22);
-    Matriz M6 = strassen(A21 - A11, B11 + B12);
-    Matriz M7 = strassen(A12 - A22, B21 + B22);
+    Matriz M1 = strassen(A11 + A22, B11 + B22, n0);
+    Matriz M2 = strassen(A21 + A22, B11, n0);
+    Matriz M3 = strassen(A11, B12 - B22, n0);
+    Matriz M4 = strassen(A22, B21 - B11, n0);
+    Matriz M5 = strassen(A11 + A12, B22, n0);
+    Matriz M6 = strassen(A21 - A11, B11 + B12, n0);
+    Matriz M7 = strassen(A12 - A22, B21 + B22, n0);
 
     // COMBINAR: Construir la matriz resultado C
     Matriz C(n, n);
